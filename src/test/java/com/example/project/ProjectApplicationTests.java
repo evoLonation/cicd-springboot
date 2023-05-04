52cf80bf76f68c1103d031286aae838df79518e8
evolonation <1838940019@qq.com>
2023-05-04 20:53:51 +0800

    test classes

--- src/test/java/com/example/project/ProjectApplicationTests.java
+++ src/test/java/com/example/project/ProjectApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.project.HelloApplication;
 
-@SpringBootTest(classes = com.example.project.HelloApplication.class)
+@SpringBootTest(classes = HelloApplication.class)
 class ProjectApplicationTests {
 
     @Test
